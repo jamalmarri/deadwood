9358602c729e0a0015d928284c09f0ef41497330
Jamal Marri <marrij@wwu.edu>
2020-02-05 23:18:12 -0800

    Add Dice class.

---
+++ src/Dice.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public final class Dice {
+    private static final Random r = new Random();
+    
+    private Dice() {}
+    
+    public static int[] roll(int amountToRoll) {
+        int[] rolls = new int[amountToRoll];
+        // Roll dice
+        return rolls;
+    }
+}
