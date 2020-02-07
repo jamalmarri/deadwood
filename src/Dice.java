f036843bdc66020b799379c7a31a3cc7d2dd411e
Jamal Marri <marrij@wwu.edu>
2020-02-06 20:46:57 -0800

    Implement rolling dice behavior.

--- src/Dice.java
+++ src/Dice.java
@@ -2,12 +2,16 @@ import java.util.Random;
 
 public final class Dice {
     private static final Random r = new Random();
-    
-    private Dice() {}
-    
+
+    private Dice() {
+    }
+
     public static int[] roll(int amountToRoll) {
         int[] rolls = new int[amountToRoll];
-        // Roll dice
+        for (int i = 0; i < rolls.length; i++) {
+            rolls[i] = r.nextInt(6) + 1;
+            System.out.println(rolls[i]);
+        }
         return rolls;
     }
 }
