50cb7d3a0477bbce32c6976b7e4ce78860fb1ddc
Jamal Marri <marrij@wwu.edu>
2020-02-06 18:32:31 -0800

    Update Javadoc comments for Part class.

--- src/Part.java
+++ src/Part.java
@@ -1,5 +1,5 @@
 /**
- * Represents a part that the player plays,
+ * Represents a Part that the Player plays,
  * either on a Card or on a Set.
  *
  * @author Jamal Marri
@@ -17,9 +17,9 @@ public class Part {
     /**
      * Class constructor.
      *
-     * @param name  the name of this part, to be displayed to the user.
-     * @param line  the line the player will 'speak', to be displayed to the user.
-     * @param level the minimum level or 'rank' the player must be to attain this part.
+     * @param name  the name of this Part, to be displayed to the user.
+     * @param line  the line the Player will 'speak', to be displayed to the user.
+     * @param level the minimum level or 'rank' the Player must be to attain this Part.
      */
     public Part(String name, String line, int level) {
         this.name = name;
@@ -28,37 +28,37 @@ public class Part {
     }
 
     /**
-     * Gets the name of this part for displaying to the user.
+     * Gets the name of this Part for displaying to the user.
      *
-     * @return the name of this part.
+     * @return the name of this Part.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Gets the line of this part for displaying to the user.
+     * Gets the line of this Part for displaying to the user.
      *
-     * @return the line for this part.
+     * @return the line for this Part.
      */
     public String getLine() {
         return line;
     }
 
     /**
-     * Gets the level of this part for verifying the player meets the minimum rank requirement.
+     * Gets the level of this Part for verifying the Player meets the minimum rank requirement.
      *
-     * @return the level requirement for this part.
+     * @return the level requirement for this Part.
      */
     public int getLevel() {
         return level;
     }
 
     /**
-     * Gets the player object currently occupying this part, if there is one.
-     * This can be helpful for ensuring two players can't get the same part.
+     * Gets the Player object currently occupying this Part, if there is one.
+     * This can be helpful for ensuring two Players can't get the same Part.
      *
-     * @return the player object associated with this part.
+     * @return the Player object associated with this Part.
      * @see Player
      */
     public Player getPlayerOnPart() {
@@ -66,11 +66,11 @@ public class Part {
     }
 
     /**
-     * Sets the player that is occupying this part, for the use of ensuring
-     * no other player can attain it, and for any other use of referencing
-     * the player object.
+     * Sets the Player that is occupying this Part, for the use of ensuring
+     * no other Player can attain it, and for any other use of referencing
+     * the Player object.
      *
-     * @param playerOnPart the player object to be assigned to this part.
+     * @param playerOnPart the Players object to be assigned to this Part.
      * @see Player
      */
     public void setPlayerOnPart(Player playerOnPart) {
