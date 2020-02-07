9dfd30ad3402f8c069c33535f547ff0ec0ae5d01
Jamal Marri <marrij@wwu.edu>
2020-02-06 18:32:52 -0800

    Update Javadoc comments for Card class.

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 
 /**
- * Represents a card in the deck for the game.
+ * Represents a Card in the deck for the game.
  * Usually, this object is interacted with through
  * the Set that it is currently associated with,
  * if applicable.
@@ -21,11 +21,11 @@ public class Card {
     /**
      * Class constructor.
      *
-     * @param name        the name of this card to be displayed to the user.
-     * @param sceneNumber the scene number of this card to be displayed to the user.
-     * @param description the description of this card to be displayed to the user.
-     * @param budget      the budget of this card to be used for acting and bonus payouts.
-     * @param parts       the HashSet of parts that this card contains.
+     * @param name        the name of this Card to be displayed to the user.
+     * @param sceneNumber the scene number of this Card to be displayed to the user.
+     * @param description the description of this Card to be displayed to the user.
+     * @param budget      the budget of this Card to be used for acting and bonus payouts.
+     * @param parts       the HashSet of parts that this Card contains.
      */
     public Card(String name, int sceneNumber, String description, int budget, HashSet<Part> parts) {
         this.name = name;
@@ -36,49 +36,49 @@ public class Card {
     }
 
     /**
-     * Gets the name of this card for displaying to the user.
+     * Gets the name of this Card for displaying to the user.
      *
-     * @return the name of this card to be displayed to the user.
+     * @return the name of this Card to be displayed to the user.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Gets the scene number of this card for displaying to the user.
+     * Gets the scene number of this Card for displaying to the user.
      *
-     * @return the scene number of this card to be displayed to the user.
+     * @return the scene number of this Card to be displayed to the user.
      */
     public int getSceneNumber() {
         return sceneNumber;
     }
 
     /**
-     * Gets the description of this card for displaying to the user.
+     * Gets the description of this Card for displaying to the user.
      *
-     * @return the description of this card to be displayed to the user.
+     * @return the description of this Card to be displayed to the user.
      */
     public String getDescription() {
         return description;
     }
 
     /**
-     * Gets the budget of this card for verifying if the player has
+     * Gets the budget of this Card for verifying if the Player has
      * successfully acted on their part, and to determine the amount
      * of dice to roll in the event of a bonus payout.
      *
-     * @return the budget of the card.
+     * @return the budget of the Card.
      */
     public int getBudget() {
         return budget;
     }
 
     /**
-     * Gets the HashSet of part objects associated with the card.
-     * This is useful for determining all of the players who are
+     * Gets the HashSet of part objects associated with the Card.
+     * This is useful for determining all of the Players who are
      * applicable to a bonus payout.
      *
-     * @return the HashSet of parts that the card contains
+     * @return the HashSet of parts that the Card contains.
      * @see HashSet
      */
     public HashSet<Part> getParts() {
