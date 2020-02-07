c63557e4635917a13ea2f2b6d869996c2bd45312
Jamal Marri <marrij@wwu.edu>
2020-02-06 19:05:56 -0800

    Add Javadoc comments for Room class.

--- src/Room.java
+++ src/Room.java
@@ -1,18 +1,45 @@
 import java.util.HashSet;
 
+/**
+ * Represents a generic Room that the Player
+ * can be in, and sometimes interact with.
+ *
+ * @author Jamal Marri
+ * @author Megan Theimer
+ * @version 1.0
+ * @see Set
+ */
 public class Room {
     private final String name;
     private final HashSet<Room> neighbors;
 
+    /**
+     * Class constructor.
+     *
+     * @param name      the name of this Room to be displayed to the user.
+     * @param neighbors the neighboring Rooms of this Room.
+     */
     public Room(String name, HashSet<Room> neighbors) {
         this.name = name;
         this.neighbors = neighbors;
     }
 
+    /**
+     * Gets the name of this Room for displaying to the user.
+     *
+     * @return the name of this Room to be displayed to the user.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the HashSet of this Room's neighbors, for checking
+     * where the Player can move from this current Room.
+     *
+     * @return the HashSet of neighboring Rooms for this Room
+     * @see HashSet
+     */
     public HashSet<Room> getNeighbors() {
         return neighbors;
     }
