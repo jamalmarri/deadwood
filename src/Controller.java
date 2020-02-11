19c1d5f24cb0e9681f25c502db81a26959f38914
Jamal Marri <marrij@wwu.edu>
2020-02-11 03:34:12 -0800

    Implement write and writeln methods. Finish implementing get and write behaviors.

--- src/Controller.java
+++ src/Controller.java
@@ -10,61 +10,114 @@ public class Controller {
 
     public void writePrompt(Player player, int playerNumber) {
         // Separator to make output easier to read
-        System.out.println("------------------------------------------------------------");
+        writeln("------------------------------------------------------------");
 
         // Print player's information
-        System.out.println("Player " + playerNumber + " | Rank: " + player.getRank() + " | Dollars: " + player.getDollars() +
+        writeln("Player " + playerNumber + " | Rank: " + player.getRank() + " | Dollars: " + player.getDollars() +
                 " | Credits: " + player.getCredits() + " | Current Space: " + player.getCurrentRoom().getName());
 
         // Print neighboring rooms of player's current room
-        System.out.print("Neighboring rooms:");
+        write("Neighboring rooms:");
         for (Room neighbor : player.getCurrentRoom().getNeighbors()) {
-            System.out.print(" " + neighbor.getName());
+            write(" " + neighbor.getName());
         }
 
-        System.out.println();
+        writeln("");
     }
 
     public void writeResponse(int action, int outputType) {
         switch (action) {
             case 0:
                 switch (outputType) {
+                    case -1:
+                        writeln("You cannot move rooms while in a movie.");
+                        break;
                     case 0:
-                        System.out.println("Moved to new room!");
+                        writeln("You moved to new room!");
                         break;
                     default:
                         break;
                 }
             case 1:
                 switch (outputType) {
+                    case -3:
+                        writeln("This movie has already been completed. Come back tomorrow.");
+                        break;
+                    case -2:
+                        writeln("You cannot take a new part while in a movie.");
+                        break;
+                    case -1:
+                        writeln("You are not on a set.");
+                        break;
+                    case 0:
+                        writeln("You took a new part!");
+                        break;
                     default:
                         break;
                 }
             case 2:
                 switch (outputType) {
+                    case -1:
+                        writeln("You are not currently in a movie.");
+                        break;
+                    case 0:
+                        writeln("You rehearsed for your part!");
+                        break;
+                    case 1:
+                        writeln("You don't need to rehearse. Your acting success is already guaranteed.");
+                        break;
                     default:
                         break;
                 }
             case 3:
                 switch (outputType) {
+                    case -1:
+                        writeln("You are not currently in a movie.");
+                        break;
+                    case 0:
+                        writeln("You successfully completed a shot! You got 2 credits.");
+                        break;
+                    case 1:
+                        writeln("You successfully completed a shot! You got 1 credit and 1 dollar.");
+                        break;
+                    case 2:
+                        writeln("You failed to complete this shot. You got 1 dollar.");
+                        break;
+                    case 3:
+                        writeln("You failed to complete this shot.");
+                        break;
                     default:
                         break;
                 }
             case 4:
                 switch (outputType) {
+                    case -2:
+                        writeln("You cancelled your upgrade.");
+                        break;
+                    case -1:
+                        writeln("You are not in the Casting Office.");
+                        break;
+                    case 0:
+                        writeln("You upgraded your rank!");
+                        break;
                     default:
                         break;
                 }
+            case 5:
+                if (outputType == 0) {
+                    writeln("Bonus money was given to all players on set!");
+                }
+                writeln("Movie completed! Come back tomorrow!");
             default:
                 break;
         }
     }
 
     public void writeScores(int[] scores) {
-        System.out.println("Game Over!");
-        System.out.println("Scores:");
+        writeln("Game Over!");
+        writeln("Scores:");
         for (int i = 0; i < scores.length; i++) {
-            System.out.println("Player " + (i + 1) + ": " + scores[i]);
+            writeln("Player " + (i + 1) + ": " + scores[i]);
         }
         scan.close();
     }
@@ -82,7 +135,7 @@ public class Controller {
 
     private int getPlayerCount() {
         // Initialize Scanner for reading user input
-        System.out.print("Enter number of players [2-8]: ");
+        write("Enter number of players [2-8]: ");
         int playerCount = 0;
 
         // Request correct input until it is received
@@ -90,12 +143,12 @@ public class Controller {
             try {
                 playerCount = scan.nextInt();
             } catch (InputMismatchException e) {
-                System.out.print("Invalid input. Please try again: ");
+                write("Invalid input. Please try again: ");
                 scan.next();
                 continue;
             }
             if (!(playerCount >= 2 && playerCount <= 8)) {
-                System.out.print("Invalid amount of players [2-8]. Please try again: ");
+                write("Invalid amount of players [2-8]. Please try again: ");
             }
         }
 
@@ -104,8 +157,8 @@ public class Controller {
     }
 
     private int getPlayerAction() {
-        System.out.println("What would you like to do?");
-        System.out.println("(move, take, rehearse, act, upgrade)");
+        writeln("What would you like to do?");
+        writeln("(move, take, rehearse, act, upgrade)");
         String input;
         while (true) {
             input = scan.nextLine();
@@ -121,20 +174,20 @@ public class Controller {
                 case "upgrade":
                     return 4;
                 default:
-                    System.out.print("Invalid action. Please try again: ");
+                    write("Invalid action. Please try again: ");
                     break;
             }
         }
     }
 
     public Room getRoom(Player player) {
-        System.out.println("Which room would you like to move to?");
+        writeln("Which room would you like to move to?");
         // Print neighboring rooms of player's current room
-        System.out.print("Neighboring rooms:");
+        write("Neighboring rooms:");
         for (Room neighbor : player.getCurrentRoom().getNeighbors()) {
-            System.out.print(" " + neighbor.getName());
+            write(" " + neighbor.getName());
         }
-        System.out.println();
+        writeln("");
         String input;
         while (true) {
             input = scan.nextLine();
@@ -143,7 +196,105 @@ public class Controller {
                     return neighbor;
                 }
             }
-            System.out.print("Invalid room name. Please try again: ");
+            write("Invalid room name. Please try again: ");
         }
+    }
+
+    public Part getPart(Player player) {
+        writeln("Which part would you like to take?");
+
+        writeln("On the card:");
+        for (Part part : ((Set) player.getCurrentRoom()).getCard().getParts()) {
+            writeln("(" + part.getLevel() + ") " + part.getName() + ": \"" + part.getLine() + "\"");
+        }
+        writeln("");
+
+        writeln("Off the card:");
+        for (Part part : ((Set) player.getCurrentRoom()).getParts()) {
+            writeln("(" + part.getLevel() + ") " + part.getName() + ": \"" + part.getLine() + "\"");
+        }
+
+        String input;
+        while (true) {
+            input = scan.nextLine();
+            for (Part part : ((Set) player.getCurrentRoom()).getCard().getParts()) {
+                if (part.getName().equals(input)) {
+                    if (player.getRank() >= part.getLevel()) {
+                        player.setOnCard(true);
+                        return part;
+                    } else {
+                        write("You are not a high enough rank for that part. Please try again: ");
+                    }
+                }
+            }
+            for (Part part : ((Set) player.getCurrentRoom()).getParts()) {
+                if (part.getName().equals(input)) {
+                    if (player.getRank() >= part.getLevel()) {
+                        player.setOnCard(false);
+                        return part;
+                    } else {
+                        write("You are not a high enough rank for that part. Please try again: ");
+                    }
+                }
+            }
+            write("Invalid part name. Please try again: ");
+        }
+    }
+
+    public int getRank(Player player) {
+        int rank = player.getRank();
+        writeln("What rank would you like to upgrade to? You are currently rank " + rank + ". Enter your current rank to cancel.");
+        for (int i = 0; i < Deadwood.upgradeCreditPrices.length; i++) {
+            writeln("Rank " + (i + 1) + ": " + Deadwood.upgradeCreditPrices[i] + " Credits or " + Deadwood.upgradeDollarPrices[i] + " Dollars.");
+        }
+        while (true) {
+            rank = scan.nextInt();
+            scan.nextLine();
+            if (rank == player.getRank()) {
+                return 0;
+            } else if (rank < player.getRank()) {
+                write("Your rank is already higher than the chosen rank. Please try again: ");
+            } else {
+                if (player.getCredits() < Deadwood.upgradeCreditPrices[rank - 1] && player.getDollars() < Deadwood.upgradeDollarPrices[rank - 1]) {
+                    write("You don't have enough credits or dollars for that rank. Please try again: ");
+                } else {
+                    writeln("What currency would you like to use to purchase this rank? (credits, dollars)");
+                    String currency;
+                    while (true) {
+                        currency = scan.nextLine();
+                        switch (currency) {
+                            case "credits":
+                                if (player.getCredits() >= Deadwood.upgradeCreditPrices[rank - 1]) {
+                                    player.setCredits(player.getCredits() - Deadwood.upgradeCreditPrices[rank - 1]);
+                                    return rank;
+                                } else {
+                                    write("You don't have enough of that currency for this rank. Please try again: ");
+                                }
+                                break;
+                            case "dollars":
+                                if (player.getDollars() >= Deadwood.upgradeDollarPrices[rank - 1]) {
+                                    player.setDollars(player.getDollars() - Deadwood.upgradeDollarPrices[rank - 1]);
+                                    return rank;
+                                } else {
+                                    write("You don't have enough of that currency for this rank. Please try again: ");
+                                }
+                                break;
+                            default:
+                                write("Invalid currency type. Please try again: ");
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void write(String output) {
+        System.out.print(output);
+    }
+
+    private void writeln(String output) {
+        write(output);
+        System.out.println();
     }
 }
