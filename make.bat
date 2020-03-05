26cc202b6a80c5c735d0d42736bc071d7937ff57
Jamal Marri <marrij@wwu.edu>
2020-03-04 18:23:33 -0800

    Update make batch script.

--- make.bat
+++ make.bat
@@ -1,6 +1,8 @@
+@echo off
+
 if not exist out mkdir out
-javac -d out\ src\*
-xcopy /Q /Y src\*.uxf out\
+javac -d out src\*.java
+xcopy /Q /Y src\*.fxml out\
 xcopy /E /Q /Y src\img out\img\
 cls
 java -cp out/ Deadwood
