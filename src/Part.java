220f839622217adf0bb0fd5af13b3e1bafee7f53
Jamal Marri <marrij@wwu.edu>
2020-03-05 23:55:51 -0800

    Update Javadoc comments for Part class.

--- src/Part.java
+++ src/Part.java
@@ -1,26 +1,39 @@
 /**
- * Represents a Part that the Player plays,
- * either on a Card or on a Set.
+ * Represents a Part that the Player plays, either on a Card or on a Set.
  *
  * @author Jamal Marri
  * @author Megan Theimer
- * @version 1.0
  * @see Card
  * @see Set
+ * @see Player
  */
 public class Part {
+    /** The name of this Part. */
     private final String name;
+
+    /** This Part's minimum level or 'rank' a Player must be to attain it. */
     private final int level;
+
+    /** This Part's x-coordinate, sometimes in relation to the Card that contains it. */
     private final int x;
+
+    /** This Part's y-coordinate, sometimes in relation to the Card that contains it. */
     private final int y;
+
+    /** The height and width a Player's dice should be when they are on this Part. */
     private final int diceSize;
+
+    /** The Player who is acting on this Part. */
     private Player playerOnPart;
 
     /**
      * Class constructor.
      *
-     * @param name  the name of this Part, to be displayed to the user.
-     * @param level the minimum level or 'rank' the Player must be to attain this Part.
+     * @param name the name of this Part
+     * @param level the minimum level or 'rank' a Player must be to attain this Part
+     * @param x this Part's x-coordinate, sometimes in relation to the Card that contains it
+     * @param y this Part's y-coordinate, sometimes in relation to the Card that contains it
+     * @param diceSize the height and width a Player's dice should be when they are on this Part
      */
     public Part(String name, int level, int x, int y, int diceSize) {
         this.name = name;
@@ -31,40 +44,54 @@ public class Part {
     }
 
     /**
-     * Gets the name of this Part for displaying to the user.
+     * Gets the name of this Part.
      *
-     * @return the name of this Part.
+     * @return the name of this Part
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Gets the level of this Part for verifying the Player meets the minimum rank requirement.
+     * Gets the level of this Part.
      *
-     * @return the level requirement for this Part.
+     * @return the level of this Part
      */
     public int getLevel() {
         return level;
     }
 
+    /**
+     * Gets this Part's x-coordinate, sometimes in relation to the Card that contains it.
+     *
+     * @return the x-coordinate of this Part
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Gets this Part's y-coordinate, sometimes in relation to the Card that contains it.
+     *
+     * @return the y-coordinate of this Part
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Gets the height and width a Player's dice should be when they are on this Part.
+     *
+     * @return the dice size
+     */
     public int getDiceSize() {
         return diceSize;
     }
 
     /**
-     * Gets the Player object currently occupying this Part, if there is one.
-     * This can be helpful for ensuring two Players can't get the same Part.
+     * Gets the Player object currently occupying this Part.
      *
-     * @return the Player object associated with this Part.
+     * @return the Player object acting on this Part
      * @see Player
      */
     public Player getPlayerOnPart() {
@@ -72,11 +99,9 @@ public class Part {
     }
 
     /**
-     * Sets the Player that is occupying this Part, for the use of ensuring
-     * no other Player can attain it, and for any other use of referencing
-     * the Player object.
+     * Sets the Player that is acting on this Part.
      *
-     * @param playerOnPart the Players object to be assigned to this Part.
+     * @param playerOnPart the Players object to be assigned to this Part
      * @see Player
      */
     public void setPlayerOnPart(Player playerOnPart) {
