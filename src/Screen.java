9cee2e123bbba4c53d2a014701e5cf9e2c867743
Jamal Marri <marrij@wwu.edu>
2020-03-05 23:59:03 -0800

    Add hiding/recentering and update Javadoc comments for Screen class.

--- src/Screen.java
+++ src/Screen.java
@@ -5,33 +5,46 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/** JavaFX Application which displays the current state of the game. */
 public class Screen extends Application {
 
+    /** The primary Stage for this Screen. */
     private Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) {
+        // Initialize local primaryStage field
         this.primaryStage = primaryStage;
+        // Attempt to load menu FXML file
         Scene menuScene = null;
         try {
             menuScene = FXMLLoader.load(getClass().getResource("menu.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        primaryStage.setScene(menuScene);
+        // Set the stage's title, and make it not resizable
         primaryStage.setTitle("Deadwood");
         primaryStage.setResizable(false);
+        // Set the stage's scene to the menu scene
+        primaryStage.setScene(menuScene);
+        primaryStage.centerOnScreen();
         primaryStage.show();
     }
 
+    /** Switches the primary Stage's scene to the game scene. */
     public void switchScene() {
+        // Attempt to load game FXML file
         Scene gameScene = null;
         try {
             gameScene = FXMLLoader.load(getClass().getResource("game.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // Hide the stage while setting the scene
+        primaryStage.hide();
+        // Set the stage's scene to the game scene
         primaryStage.setScene(gameScene);
         primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
