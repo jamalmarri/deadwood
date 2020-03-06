3f2dc0600af2e50043ea959c8458b393cd7deca7
Jamal Marri <marrij@wwu.edu>
2020-03-05 23:49:11 -0800

    Update Javadoc comments for Card class.

--- src/Card.java
+++ src/Card.java
@@ -1,28 +1,34 @@
 import java.util.HashSet;
 
 /**
- * Represents a Card in the deck for the game.
- * Usually, this object is interacted with through
- * the Set that it is currently associated with,
- * if applicable.
+ * Represents a Card in the deck for the game. This object is interacted with through the Set that
+ * it is currently placed in, and contains its own HashSet of Parts.
  *
  * @author Jamal Marri
  * @author Megan Theimer
- * @version 1.0
  * @see Set
+ * @see Part
  */
 public class Card {
+    /** The name for the movie represented on this Card. */
     private final String name;
+
+    /** The budget for the movie represented on this Card. */
     private final int budget;
+
+    /** The path to the image of this Card to display on the Screen. */
     private final String pathToCard;
+
+    /** The HashSet of Parts that this Card contains. */
     private final HashSet<Part> parts;
 
     /**
      * Class constructor.
      *
-     * @param name   the name of this Card to be displayed to the user.
-     * @param budget the budget of this Card to be used for acting and bonus payouts.
-     * @param parts  the HashSet of Parts that this Card contains.
+     * @param name the name for the movie represented on this Card
+     * @param budget the budget for the movie represented on this Card
+     * @param pathToCard the path to the image of this Card to display on the Screen
+     * @param parts the HashSet of Parts that this Card contains
      * @see HashSet
      */
     public Card(String name, int budget, String pathToCard, HashSet<Part> parts) {
@@ -33,46 +39,48 @@ public class Card {
     }
 
     /**
-     * Gets the name of this Card for displaying to the user.
+     * Gets the name for the movie represented on this Card.
      *
-     * @return the name of this Card to be displayed to the user.
+     * @return the name of this Card
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Gets the budget of this Card for verifying if the Player has
-     * successfully acted on their part, and to determine the amount
-     * of dice to roll in the event of a bonus payout.
+     * Gets the budget for the movie represented on this Card.
      *
-     * @return the budget of this Card.
+     * @return the budget of this Card
      */
     public int getBudget() {
         return budget;
     }
 
+    /**
+     * Gets the path to the image of this Card.
+     *
+     * @return the path to the image of this Card
+     */
     public String getPathToCard() {
         return pathToCard;
     }
 
     /**
-     * Gets the HashSet of part objects associated with this Card.
-     * This is useful for determining all of the Players who are
-     * applicable to a bonus payout.
+     * Gets the HashSet of Part objects associated with this Card.
      *
-     * @return the HashSet of parts that this Card contains.
+     * @return the HashSet of Parts that this Card contains
      * @see HashSet
+     * @see Part
      */
     public HashSet<Part> getParts() {
         return parts;
     }
 
     /**
-     * Adds a new Part to this Card's 'parts' HashSet, for when
-     * the game adds every Card's Parts.
+     * Adds a new Part to this Card's 'parts' HashSet.
      *
-     * @param part the new part to be added to the 'parts' HashSet.
+     * @param part the new Part to be added to the 'parts' HashSet
+     * @see Part
      */
     public void addPart(Part part) {
         parts.add(part);
