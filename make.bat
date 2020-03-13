aa0926d10c2131efa8ff7ef4565b4fb30fe63011
Jamal Marri <marrij@wwu.edu>
2020-03-13 13:28:34 -0700

    Add del and rmdir statements for self-cleanup.

--- make.bat
+++ make.bat
@@ -2,7 +2,10 @@
 
 if not exist out mkdir out
 javac -d out src\*.java
-xcopy /Q /Y src\*.fxml out\
-xcopy /E /Q /Y src\img out\img\
+xcopy /Q /Y src\*.fxml out\ >nul
+xcopy /E /Q /Y src\img out\img\ >nul
 cls
 java -cp out/ Deadwood
+rmdir /S /Q out\img\ >nul
+del /S /Q out\*.class >nul
+del /S /Q out\*.fxml >nul
